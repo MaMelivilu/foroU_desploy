b9c6ab9add4ac947cdff0b31bd02e31c205c5768
MaMelivilu <127501167+MaMelivilu@users.noreply.github.com>
2025-11-17 16:58:04 -0300

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,11 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // ⚡ Desactiva errores por 'any'
+      // Opcional: si quieres, puedes desactivar warnings de variables no usadas temporalmente
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
   },
 ];
 
